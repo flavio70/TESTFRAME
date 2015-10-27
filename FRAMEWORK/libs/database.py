ff8365e4e05106c7705ebd66ac28e852dbd73b16
ippolf <ippolf@it052016.it.alcatel-lucent.com>
2015-10-27 11:35:38 +0100

    added Django database support

---
+++ FRAMEWORK/libs/database.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+###############################################################################
+# MODULE: database.py
+# 
+# AUTHOR: F.Ippolito
+# DATE  : 08/09/2015
+#
+# this module imports the DB APIs from K@TE DJANGO Framework
+# and provide all K@TE database's objects access and management to the K@TE test
+# developer by using DJANGO DB access
+###############################################################################
+
+
+import os
+
+# Getting Django Setting (settings.py) and setting basic configuration for DJANGO DB connection
+# settings.py file must be in ./DB_API_CONF folder
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KateLibs.DB_API_CONF.settings")
+
+#import all objects defined in models.py
+#models.py module must be placed in ./DB_API_LIB folder
+from KateLibs.DB_API_LIB.models import *
+
+
+
+
+#global a
+#a=TTest
+#print(TTest.objects.all())
+#print(a)
