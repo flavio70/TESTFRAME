1a5db15573a923ed8db7ffc33e04c36ded5f1d85
ghelfc <ghelfc@it052016.it.alcatel-lucent.com>
2016-03-31 14:38:49 +0200

    Fix issue on plugin_bm.py Improving KateEqptReport.py and scan_eqpt.py

--- FRAMEWORK/scripts/KateEqptReport.py
+++ FRAMEWORK/scripts/KateEqptReport.py
@@ -48,10 +48,55 @@ def getEqptSerial(allSer, n):
     return msg
 
 
+def getBoardTypeList():
+    cursor = connection.cursor()
+    query = "SELECT id_board_type,name FROM T_BOARD_TYPE"
+    cursor.execute(query)
+
+    result = cursor.fetchall()
+
+    board_type_list = {}
+
+    if len(result) > 0:
+        for row in result:
+            board_type_list[row[0]] = row[1]
+
+    return board_type_list
+
+
+def getEqptBoards(eqpt_id, eqpt_type, board_type_list):
+    if not eqpt_type in ['1850TSS320', '1850TSS320H', '1850TSS160', '1850TSS160H', 'TSS-320T']:
+        return None
+
+    cursor = connection.cursor()
+
+    query = ' '.join( ( "SELECT T_BOARD_TYPE_id_board_type, slot",
+                        "FROM   T_BOARDS",
+                        "WHERE  T_EQUIPMENT_id_equipment='{}'".format(eqpt_id) ) )
+    cursor.execute(query)
+
+    result = cursor.fetchall()
+
+    list_boards = None
+
+    if len(result) > 0:
+        for row in result:
+            slot = row[1]
+            board_name = board_type_list[row[0]]
+
+            if list_boards is None:
+                list_boards = "{}:{}".format(slot, board_name)
+            else:
+                list_boards = "{} {}:{}".format(list_boards, slot, board_name)
+
+    return list_boards
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--all", help="get full info", action="store_true")
+    parser.add_argument("--serials", help="get serials info", action="store_true")
+    parser.add_argument("--boards", help="get boards info", action="store_true")
     args = parser.parse_args()
 
     allIP  = TNet.objects.all()
@@ -60,12 +105,14 @@ if __name__ == "__main__":
     tabLocation = TLocation
     tabEqptType = TEquipType
 
+    board_type_list = getBoardTypeList()
+
     if True:
         for r in TEquipment.objects.all():
             eType = getEqptTypeName(tabEqptType, r.t_equip_type_id_type.id_type)
             eLoc  = getEqptLocation(tabLocation, r.t_location_id_location.id_location)
             eIP   = getEqptIP(allIP, r.id_equipment)
-            if args.all:
+            if args.serials:
                 eSer = getEqptSerial(allSer, r.id_equipment)
                 res='{:5s} {:12s} {:10s} {:20s} {:20s} {:16} {:s}'.format(
                         str(r.id_equipment),
@@ -77,14 +124,30 @@ if __name__ == "__main__":
                         eSer
                     )
             else:
-                res='{:5s} {:12s} {:10s} {:20s} {:20s} {:16}'.format(
-                        str(r.id_equipment),
-                        eType,
-                        eLoc,
-                        str(r.name),
-                        eIP,
-                        str(r.owner)
-                    )
+                if args.boards:
+                    board_list = getEqptBoards(r.id_equipment, eType, board_type_list)
+                else:
+                    board_list = None
+
+                if board_list is None:
+                    res='{:5s} {:12s} {:10s} {:20s} {:20s} {:16}'.format(
+                            str(r.id_equipment),
+                            eType,
+                            eLoc,
+                            str(r.name),
+                            eIP,
+                            str(r.owner)
+                        )
+                else:
+                    res='{:5s} {:12s} {:10s} {:20s} {:20s} {:16} [{}]'.format(
+                            str(r.id_equipment),
+                            eType,
+                            eLoc,
+                            str(r.name),
+                            eIP,
+                            str(r.owner),
+                            board_list
+                        )
             print(res)
     else:
         if True:
