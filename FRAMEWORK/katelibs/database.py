83ac900a02e106cab1152b4e15529e027a07dc23
ghelfc <ghelfc@it052016.it.alcatel-lucent.com>
2016-04-27 08:22:00 +0200

    kautotest: adding management of Node Locking (guaranted)

--- FRAMEWORK/katelibs/database.py
+++ FRAMEWORK/katelibs/database.py
@@ -13,7 +13,7 @@
 
 import os, ast
 import django
-from django.db import connection
+from django.db import connection, transaction
 from katelibs.kprint import *
 
 # Getting Django Setting (settings.py) and setting basic configuration for DJANGO DB connection
@@ -31,144 +31,168 @@ from katelibs.DB_API_LIB.models import *
 
 
 def check_DB_Author(author):
-	""" check the presence of author  in the right table """
-	if author.strip() == '':return False
-	try:
-		cursor = connection.cursor()
-		cursor.execute("SELECT count(id) as myCount from auth_user WHERE username='"+author.strip()+"'")
-		row=cursor.fetchone()
-		if row[0]==0:
-			return False
-		else:
-			return True
-	except Exception as eee:
-		print(str(eee))
-		return False
+    """ check the presence of author  in the right table """
+    if author.strip() == '':return False
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT count(id) as myCount from auth_user WHERE username='"+author.strip()+"'")
+        row=cursor.fetchone()
+        if row[0]==0:
+            return False
+        else:
+            return True
+    except Exception as eee:
+        print(str(eee))
+        return False
 
 
 
 
 def check_DB_Topology(topology):
-	""" check the presence of topology  in the right table """
-	if topology.strip() == '':return False
-	try:
-		cursor = connection.cursor()
-		cursor.execute("SELECT count(title) as myCount from T_TOPOLOGY WHERE id_topology='"+topology.strip()+"'")
-		row=cursor.fetchone()
-		if row[0]==0:
-			return False
-		else:
-			return True
-	except Exception as eee:
-		print(str(eee))
-		return False
+    """ check the presence of topology  in the right table """
+    if topology.strip() == '':return False
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT count(title) as myCount from T_TOPOLOGY WHERE id_topology='"+topology.strip()+"'")
+        row=cursor.fetchone()
+        if row[0]==0:
+            return False
+        else:
+            return True
+    except Exception as eee:
+        print(str(eee))
+        return False
 
 
 
 def get_eqpt_nodes(node_list):
-	'''
-	get 'eqpt' type node id from a generic node list
-	'''
-	_querystr = None
-	for node in node_list:
-		if not _querystr:
-			_querystr = 'id_equipment='+ str(node)
-		else:
-			_querystr = _querystr + ' or id_equipment='+ str(node)
-	eqpt_list=[]
-	try:	
-		cursor = connection.cursor()
-		cursor.execute("SELECT id_equipment FROM KATE.T_EQUIPMENT JOIN KATE.T_EQUIP_TYPE on (T_EQUIP_TYPE_id_type = id_type) WHERE family = 'EQPT' and (" + _querystr + ")")
-		for myitem in cursor.fetchall():
-			eqpt_list.append(myitem[0])
-		return eqpt_list
-	except Exception as eee:
-		kprint_fail(str(eee))
-		return eqpt_list
+    '''
+    get 'eqpt' type node id from a generic node list
+    '''
+    _querystr = None
+    for node in node_list:
+        if not _querystr:
+            _querystr = 'id_equipment='+ str(node)
+        else:
+            _querystr = _querystr + ' or id_equipment='+ str(node)
+    eqpt_list=[]
+    try:    
+        cursor = connection.cursor()
+        cursor.execute("SELECT id_equipment FROM KATE.T_EQUIPMENT JOIN KATE.T_EQUIP_TYPE on (T_EQUIP_TYPE_id_type = id_type) WHERE family = 'EQPT' and (" + _querystr + ")")
+        for myitem in cursor.fetchall():
+            eqpt_list.append(myitem[0])
+        return eqpt_list
+    except Exception as eee:
+        kprint_fail(str(eee))
+        return eqpt_list
+
 
 def lock_eqpt_nodes(node_list):
-	'''
-	try to lock the node_list changing the insue value
-	'''
-	res=False
-	try:
-		localtab = TEquipment
-		if len(node_list) == len(localtab.objects.filter(id_equipment__in=node_list, inuse=0)):
-			#in this case all nodes in the list ar free and can be locked
-			localtab.objects.filter(id_equipment__in=node_list).update(inuse=1)
-			res=True
-			kprint_green('Nodes '+ str(node_list) + ' locked!')
-		else:
-			#in this case at least one node is used by others and the node_list cannot be locked
-			kprint_warning('WARNING!! Nodes '+ str(node_list) + ' CANNOT be locked! One or more node already in use.')
-		return res
-	except Exception as eee:
-		kprint_fail(str(eee))
-		return res
+    """
+    Try to lock the node_list changing the 'inUse' value
+    The procedure fails (with no-change on DB) if almost a node is already locked
+    """
+    sql_node_list = ""
+    for node_id in node_list:
+        if sql_node_list == "":
+            sql_node_list = "('{}'".format(node_id)
+        else:
+            sql_node_list = "{},'{}'".format(sql_node_list, node_id)
+    sql_node_list = "{})".format(sql_node_list)
+
+    res = False
+    with transaction.atomic():
+        cursor = connection.cursor()
+        query = ' '.join( ( "SELECT id_equipment, inUse",
+                            "FROM T_EQUIPMENT",
+                            "WHERE id_equipment IN {}".format(sql_node_list),
+                            "      AND inUse='1'" ) )
+        cursor.execute(query)
+        result = cursor.fetchall()
+        if len(result) > 0:
+            print("WARNING: following node(s) already locked:")
+            for row in result:
+                print(row[0])
+        else:
+            query = "UPDATE T_EQUIPMENT SET inUse='1' WHERE id_equipment IN {}".format(sql_node_list)
+            cursor.execute(query)
+            print("ALL REQUIRED NODE(S) LOCKED")
+            res = True
+
+    return res
+
 
 def unlock_eqpt_nodes(node_list):
-	'''
-	try to unlock the node_list changing the insue value
-	'''
-	res=False
-	try:
-		localtab = TEquipment 
-		localtab.objects.filter(id_equipment__in=node_list).update(inuse=0)
-		res=True
-		kprint_green('Nodes '+ str(node_list) + ' unlocked!')
-		return res
-	except Exception as eee:
-		kprint_fail(str(eee))
-		return res
+    """
+    Unlock the equipment in node_list changing the inUse value
+    """
+    sql_node_list = ""
+    for node_id in node_list:
+        if sql_node_list == "":
+            sql_node_list = "('{}'".format(node_id)
+        else:
+            sql_node_list = "{},'{}'".format(sql_node_list, node_id)
+    sql_node_list = "{})".format(sql_node_list)
+
+    res = False
+    with transaction.atomic():
+        cursor = connection.cursor()
+        query = "UPDATE T_EQUIPMENT SET inUse='0' WHERE id_equipment IN {}".format(sql_node_list)
+        cursor.execute(query)
+        print("ALL REQUIRED NODE(S) UNLOCKED")
+        res = True
+
+    return res
+
 
 def get_RunTime_table(ID):
-	rowg=None
-	try:	
-		localtab  = TRuntime
-		rowg = localtab.objects.filter(id_run=ID)
-		return rowg
-	except Exception as eee:
-		kprint_fail(str(eee))
-		return rowg
+    rowg=None
+    try:    
+        localtab  = TRuntime
+        rowg = localtab.objects.filter(id_run=ID)
+        return rowg
+    except Exception as eee:
+        kprint_fail(str(eee))
+        return rowg
 
 def set_RunTime_status(ID,field_value):
-	try:	
-		localtab  = TRuntime
-		rowg = localtab.objects.filter(id_run=ID).update(status=field_value)
-	except Exception as eee:
-		kprint_fail(str(eee))
-		return
+    try:    
+        localtab  = TRuntime
+        rowg = localtab.objects.filter(id_run=ID).update(status=field_value)
+    except Exception as eee:
+        kprint_fail(str(eee))
+        return
 
 def set_RunTime_job_iteration(ID,field_value):
-	try:	
-		localtab  = TRuntime
-		rowg = localtab.objects.filter(id_run=ID).update(job_iteration=field_value)
-	except Exception as eee:
-		kprint_fail(str(eee))
-		return
+    try:    
+        localtab  = TRuntime
+        rowg = localtab.objects.filter(id_run=ID).update(job_iteration=field_value)
+    except Exception as eee:
+        kprint_fail(str(eee))
+        return
 
 def new_RunTime_entry(jobname, buildnum, jksowner, newstatus):
-	try:	
-		newEntry = TRuntime(job_name=jobname, job_iteration=buildnum, owner=jksowner, status=newstatus, errcount=0, runcount=0)
-		newEntry.save()
-		return newEntry.id_run
-	except Exception as eee:
-		kprint_fail(str(eee))
-		return 0
+    try:    
+        newEntry = TRuntime(job_name=jobname, job_iteration=buildnum, owner=jksowner, status=newstatus, errcount=0, runcount=0)
+        newEntry.save()
+        return newEntry.id_run
+    except Exception as eee:
+        kprint_fail(str(eee))
+        return 0
 
 def set_RunTime_errCount(ID,field_value):
-	try:	
-		localtab  = TRuntime
-		rowg = localtab.objects.filter(id_run=ID).update(errcount=field_value)
-	except Exception as eee:
-		kprint_fail(str(eee))
-		return
+    try:    
+        localtab  = TRuntime
+        rowg = localtab.objects.filter(id_run=ID).update(errcount=field_value)
+    except Exception as eee:
+        kprint_fail(str(eee))
+        return
 
 def set_RunTime_runCount(ID,field_value):
-	try:	
-		localtab  = TRuntime
-		rowg = localtab.objects.filter(id_run=ID).update(runcount=field_value)
-	except Exception as eee:
-		kprint_fail(str(eee))
-		return
+    try:    
+        localtab  = TRuntime
+        rowg = localtab.objects.filter(id_run=ID).update(runcount=field_value)
+    except Exception as eee:
+        kprint_fail(str(eee))
+        return
 
--- FRAMEWORK/scripts/kautotest.py
+++ FRAMEWORK/scripts/kautotest.py
@@ -6,166 +6,184 @@ Module used to drive Jenkins jobs
 @AUTHOR: F.Ippolito
 @DATE  : 02/09/2015
 """
-import os, sys, time
-import json, glob
+
+import os
+import sys
+import json
+import glob
 import argparse
 import signal
-from git import Repo
-from katelibs.kunit import Kunit
-from katelibs.database import *
-from katelibs.kprint import *
-from django.db import connection
+
+from git                import Repo
+from katelibs.kunit     import Kunit
+from katelibs.database  import lock_eqpt_nodes, unlock_eqpt_nodes
+from katelibs.database  import get_RunTime_table, new_RunTime_entry
+from katelibs.database  import set_RunTime_status, set_RunTime_job_iteration
+from katelibs.database  import set_RunTime_errCount, set_RunTime_runCount
 
 
 gRunId = ''
 
 def handler_signal(signum, stack):
-		print ('Received:%s SIGTERM Signal/n...Aborting K@TE suite execution...'%signum)
-		set_RunTime_status(gRunId, 'ABORTED')
-		print('...Done!!')
+    print ('Received:%s SIGTERM Signal/n...Aborting K@TE suite execution...'%signum)
+    set_RunTime_status(gRunId, 'ABORTED')
+    print('...Done!!')
 
 
 def printFRMWRKdata(JenkinsHome, JobWorkspace):
-	"""
-	print Framework environemnt data into Kunit format
-	"""
-	try:
-		myRepo = Repo(JenkinsHome + '/KateRepo')
-		git = myRepo.git
-		frmwrkVersionLong = git.log('--decorate', '-1') 
-	except:
-		frmwrkVersionLong = 'Git infos not available' 
-	try:
-		frmwrkVersionShort = git.describe('--tags')
-	except:
-		frmwrkVersionShort = 'tags Not Available'
-	print('K@TE Framework Commit used for test: ' + frmwrkVersionLong + '/n')
-	print('API version: ' + frmwrkVersionShort)
-	xmlReportPath = JobWorkspace + '/test-reports'
-	xmlReportFile = 'EnvSettings.FrameworkAPI.XML'
-	r = Kunit(xmlReportPath,xmlReportFile)
-	r.start_time()
-	r.add_success(None, "KATE API: " + frmwrkVersionShort, None, frmwrkVersionLong)
-	r.frame_close()
+    """
+    print Framework environemnt data into Kunit format
+    """
+    try:
+        myRepo = Repo(JenkinsHome + '/KateRepo')
+        git = myRepo.git
+        frmwrkVersionLong = git.log('--decorate', '-1')
+    except:
+        frmwrkVersionLong = 'Git infos not available'
+    try:
+        frmwrkVersionShort = git.describe('--tags')
+    except:
+        frmwrkVersionShort = 'tags Not Available'
+    print('K@TE Framework Commit used for test: ' + frmwrkVersionLong + '/n')
+    print('API version: ' + frmwrkVersionShort)
+    xmlReportPath = JobWorkspace + '/test-reports'
+    xmlReportFile = 'EnvSettings.FrameworkAPI.XML'
+    r = Kunit(xmlReportPath,xmlReportFile)
+    r.start_time()
+    r.add_success(None, "KATE API: " + frmwrkVersionShort, None, frmwrkVersionLong)
+    r.frame_close()
 
 def get_file_bytype(file_path,file_type):
-	'''
-	get al files from a folder with a selected type
-	'''
-	_res=[file for file in glob.glob(os.path.join(file_path, '*.' + file_type))]
-	return _res
+    '''
+    get al files from a folder with a selected type
+    '''
+    _res=[file for file in glob.glob(os.path.join(file_path, '*.' + file_type))]
+    return _res
+
 
 def get_job_nodelist(file_list):
-	'''
-	get the K@TE node list from presets
-	file_list: [Names of prs files]
-	'''
-	res = []
-	for file in file_list:
-		_file_descr = open(os.path.abspath(file))	
-		_prs_values = json.load(_file_descr)
-		_file_descr.close()
-		for key, values in _prs_values.items():
-			if '_' not in key and values not in res: res.append(values)
-	return res
+    '''
+    get the K@TE node list from presets
+    file_list: [Names of prs files]
+    '''
+    res = []
+    for the_file in file_list:
+        _file_descr = open(os.path.abspath(the_file))
+        _prs_values = json.load(_file_descr)
+        _file_descr.close()
+        for elem in _prs_values:
+            test_preset = { x[0] : x[1] for x in _prs_values[elem] }
+            if test_preset['TYPE'] in ['1850TSS320', '1850TSS320H', '1850TSS160', '1850TSS160H', 'TSS-320T']:
+                res.append(test_preset['ID'])
+    return list(set(res))
+
 
 def guaranted_flow(args):
-	"""
-	main flow control in case of policy = GUA
-	in this case all suite resources shall be locked before suite execution and relased at the end
-	if one or more resource are not available the entire suite will be stopped
-	"""
-	prs_list = get_file_bytype(args.JobWorkspace,'prs')
-	#node_list = get_job_nodelist(prs_list)
-	#kprint_info('Getting Job Node indexes... '+ str(node_list))
-	#eqpt_list = get_eqpt_nodes(node_list)	
-	#kprint_info('Getting EQPT Node indexes... '+ str(eqpt_list))
-	#kprint_info('Locking EQPT Items...')
-	#if not lock_eqpt_nodes(eqpt_list): sys.exit('Exiting ... \nNodes cannot be locked for execution')
-	err_count = 0
-	run_count = 0
-	try:
-		#setting runtime entry to running 
-		if args.DBMode : 
-			set_RunTime_status(args.KateRunId, 'RUNNING')
-			set_RunTime_job_iteration(args.KateRunId, args.jobBuildN)
-		else:
-			args.KateRunId = new_RunTime_entry(args.jobName, args.jobBuildN, args.UserId, 'RUNNING')
-			print ('new RunId : ' + str(args.KateRunId))
+    """
+    main flow control in case of policy = GUA
+    in this case all suite resources shall be locked before suite execution and relased at the end
+    if one or more resource are not available the entire suite will be stopped
+    """
+    prs_list = get_file_bytype(args.JobWorkspace,'prs')
+    print("prs_list = {}".format(prs_list))
+    node_list = get_job_nodelist(prs_list)
+    if not lock_eqpt_nodes(node_list):
+        sys.exit('Exiting ... \nNodes cannot be locked for execution')
 
-		with open(args.inputSuite) as f:
-			for line in f:
-				run_count +=1
-				_a=os.system(line)
-				#time.sleep(60)
-				if _a != 0:
-					err_count +=1 
-					set_RunTime_status(args.KateRunId, 'FAILING')
-					set_RunTime_errCount(args.KateRunId, err_count)
-					#time.sleep(60)
-				set_RunTime_runCount(args.KateRunId, run_count)
-		set_RunTime_status(args.KateRunId, 'FAILED') if err_count > 0 else set_RunTime_status(args.KateRunId, 'COMPLETED')
-		#unlock_eqpt_nodes(eqpt_list)
-		print('End of execution')
+    #print("DEBUG MODE - EXIT")
+    #sys.exit(0)
 
-	except Exception as eee:
-		print('Exception Detected: ' + str(eee))
-		#unlock_eqpt_nodes(eqpt_list)
-		set_RunTime_status(args.KateRunId, 'ABORTED')
+    err_count = 0
+    run_count = 0
+
+    try:
+        #setting runtime entry to running
+        if args.DBMode :
+            set_RunTime_status(args.KateRunId, 'RUNNING')
+            set_RunTime_job_iteration(args.KateRunId, args.jobBuildN)
+        else:
+            args.KateRunId = new_RunTime_entry(args.jobName, args.jobBuildN, args.UserId, 'RUNNING')
+            print ('new RunId : ' + str(args.KateRunId))
+
+        with open(args.inputSuite) as f:
+            for line in f:
+                print("ESEGUO: [{}]".format(line))
+                run_count += 1
+                _a = os.system(line)
+                if _a != 0:
+                    err_count += 1
+                    set_RunTime_status(args.KateRunId, 'FAILING')
+                    set_RunTime_errCount(args.KateRunId, err_count)
+                set_RunTime_runCount(args.KateRunId, run_count)
+
+        if err_count > 0:
+            set_RunTime_status(args.KateRunId, 'FAILED')
+        else:
+            set_RunTime_status(args.KateRunId, 'COMPLETED')
+
+        unlock_eqpt_nodes(eqpt_list)
+        print('End of execution')
+
+    except Exception as eee:
+        print('Exception Detected: ' + str(eee))
+        set_RunTime_status(args.KateRunId, 'ABORTED')
+        unlock_eqpt_nodes(eqpt_list)
+
 
 def besteffort_flow(args):
-	"""
-	Main flow control in case of policy = BE
-	Only Tests with evailable resources will be executed, others will be skipped
-	"""
-	print('Exiting ... \nBest effort case not yet implemented')
+    """
+    Main flow control in case of policy = BE
+    Only Tests with evailable resources will be executed, others will be skipped
+    """
+    print('Exiting ... \nBest effort case not yet implemented')
+
 
 def main():
-	"""
-	Main loop
-	"""
-	global gRunId
-	parser = argparse.ArgumentParser()
-	parser.add_argument("inputSuite", help="The suite test file to be processed")
-	parser.add_argument("jobName", help="Jenkins Job Name", nargs='?', default='Local')
-	parser.add_argument("jobBuildN", help="Jenkins Job Build Number", nargs='?', default=0)
-	parser.add_argument("KateRunId", help="K@TE istance ID (filled automatically by K@TE interface)", nargs='?', default=None)
-	parser.add_argument("--UserId", help="K@TE UserId", nargs='?', default='Local')
-	parser.add_argument("--policy", help="K@TE suite policy", nargs='?', choices=['GUA','BE'], default='GUA')
-	
-	args = parser.parse_args()
-	print(args)
-	args.DBMode = False
-	gRunId = args.KateRunId
-	
-	signal.signal(signal.SIGTERM, handler_signal)
- 
-	try:
-		args.JenkinsHome = os.environ['JENKINS_HOME']
-		args.JobWorkspace = os.environ['WORKSPACE']
-		printFRMWRKdata(args.JenkinsHome, args.JobWorkspace)
-	except Exception as e:
-		print(str(e))
-		#sys.exit('Exiting ... \nError getting os environment JENKINS Data')
-		print('JENKINS ENV NOT DETECTED\nASSUME you are Calling me for DEBUG SCOPE...')
-		args.JobWorkspace = input("Please enter equivalent workspace: ")
-		args.JenkinsHome = ''
-		print ('workspace set to ' + args.JobWorkspace)
+    """
+    Main loop
+    """
+    global gRunId
 
-	
-	print('Checking K@TE DB RunTime Entries...')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inputSuite", help="The suite test file to be processed")
+    parser.add_argument("jobName", help="Jenkins Job Name", nargs='?', default='Local')
+    parser.add_argument("jobBuildN", help="Jenkins Job Build Number", nargs='?', default=0)
+    parser.add_argument("KateRunId", help="K@TE istance ID (filled automatically by K@TE interface)", nargs='?', default=None)
+    parser.add_argument("--UserId", help="K@TE UserId", nargs='?', default='Local')
+    parser.add_argument("--policy", help="K@TE suite policy", nargs='?', choices=['GUA','BE'], default='GUA')
 
-	row_group=get_RunTime_table(args.KateRunId)
-	print('Entries: '+ str(row_group))
-	
-	if row_group.exists(): args.DBMode =True
-	print('K@TE Mode set to: ' + str(args.DBMode))
-	print('Processing suite; ', args.inputSuite)
+    args = parser.parse_args()
+    print(args)
+    args.DBMode = False
+    gRunId = args.KateRunId
 
-	functionName = {'GUA':guaranted_flow,
-								 'BE':besteffort_flow,
-								 }
-	functionName[args.policy](args)
+    signal.signal(signal.SIGTERM, handler_signal)
+
+    try:
+        args.JenkinsHome = os.environ['JENKINS_HOME']
+        args.JobWorkspace = os.environ['WORKSPACE']
+        printFRMWRKdata(args.JenkinsHome, args.JobWorkspace)
+    except Exception as e:
+        print(str(e))
+        print('JENKINS ENV NOT DETECTED\nASSUME you are Calling me for DEBUG SCOPE...')
+        args.JobWorkspace = input("Please enter equivalent workspace: ")
+        args.JenkinsHome = ''
+        print ('workspace set to ' + args.JobWorkspace)
+
+    print('Checking K@TE DB RunTime Entries...')
+
+    row_group=get_RunTime_table(args.KateRunId)
+    print('Entries: '+ str(row_group))
+
+    if row_group.exists():
+        args.DBMode =True
+
+    print('K@TE Mode set to: ' + str(args.DBMode))
+    print('Processing suite; ', args.inputSuite)
+
+    functionName = {'GUA':guaranted_flow, 'BE':besteffort_flow, }
+    functionName[args.policy](args)
+
 
 if __name__ == "__main__":
-	main()
+    main()
