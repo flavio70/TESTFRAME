95ef81deab023d7e2129ab466341f8becd8424fe
ippolf <ippolf@it052016.it.alcatel-lucent.com>
2017-03-30 17:04:51 +0200

    added python logging framewrok capability

---
+++ FRAMEWORK/katelibs/__init__.py
@@ -0,0 +1,8 @@
+
+__all__ = ['frmkLog','TestCase','Eqpt1850TSS320','InstrumentONT','SWP1850TSS']
+
+from ._config import frmkLog
+from .testcase          import TestCase
+from .eqpt1850tss320    import Eqpt1850TSS320
+from .instrumentONT     import InstrumentONT
+from .swp1850tss320     import SWP1850TSS
---
+++ FRAMEWORK/katelibs/_config.py
@@ -0,0 +1,27 @@
+import os
+import json
+import logging
+import logging.config
+
+class _logConst():
+
+    PKG_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = PKG_DIR + '/..'
+    LOG_DIR = BASE_DIR + '/logs'
+    LOG_SETTINGS = BASE_DIR + '/logging.json'
+    ERROR_LOG = LOG_DIR +'/error.log'
+    MAIN_LOG = LOG_DIR +'/main.log'
+
+class frmkLog():
+    
+    def __init__(self):
+        c=_logConst()
+        with open(c.LOG_SETTINGS,"r",encoding="utf-8") as fd:
+            D = json.load(fd)
+            D.setdefault('version',1)
+            logging.config.dictConfig(D)
+
+    def getLogger(self,name):
+        
+        return logging.getLogger(name)
+        
